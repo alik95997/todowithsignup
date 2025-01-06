330629d61c802dd93365f2d793d143f3308794cc
alik95997 <88495045+alik95997@users.noreply.github.com>
2025-01-06 10:30:06 +0500

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,18 +1,32 @@
-import { app, auth, signInWithEmailAndPassword } from "./firebase.js"
+import { auth, db, createUserWithEmailAndPassword, setDoc, doc } from "./firebase.js";  // Ensure correct import path
+
+// Selecting elements
+const name = document.querySelector("#name");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
-const signinForm = document.querySelector("#signinForm");
-const signin = async (event) => {
-    event.preventDefault();
-    try {
-        const response = await signInWithEmailAndPassword(auth, email.value, password.value);
-        alert("Login succesfully");
-        console.log()
-        window.location.replace("./todo.html")
-    } catch (error) {
-        console.log(error.message)
-        alert("error", error.code)
-    }
+const cpassword = document.querySelector("#cpassword");
+const signupForm = document.querySelector("#signupForm");
 
-}
-signinForm.addEventListener("submit", signin)
+const signup = async (event) => {
+    event.preventDefault();
+
+
+    if (password.value !== cpassword.value) {
+        alert("Passwords do not match.");
+        return;
+    }
+    try {
+        const response = await createUserWithEmailAndPassword(auth, email.value, password.value);
+        await setDoc(doc(db, "user", response.user.uid), {
+            firstName: name.value,
+            email: email.value
+        });
+        alert("account created sucessfully")
+        window.location.replace("./index.html")
+    } catch (error) {
+        console.error("Error:", error);
+        alert(error.code)
+    }
+};
+
+signupForm.addEventListener("submit", signup);
