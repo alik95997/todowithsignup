0a83a385f98c03f90f994458e88aa43cb3701b56
alik95997 <88495045+alik95997@users.noreply.github.com>
2025-01-09 10:52:45 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,9 @@
-// var input = document.getElementById("input");
-// var UI = document.getElementById("notesParent");
-// var arr = [];
-// var textOutput = document.getElementById("textOutput");
-
-// function renderUI() {
-//     arr.unshift(input.value);
-//     localStorage.setItem("key", JSON.stringify(arr));
-//     var a = JSON.parse(localStorage.getItem("key"));
-
-//     var notes = `<div class="notes">
-//                 <div class="notesText">
-//                     <p id="textOutput">${a[0]}</p>
-//                 </div>
-//                 <div class=".icons-btn">
-//                     <button onclick="editNotesText(this)"><i class="fa-regular fa-pen-to-square"></i></button>
-//                     <button onclick="deleteNotes(this)"><i class="fa-solid fa-trash"></i></button>
-//                 </div>
-//             </div>`
-//     UI.innerHTML += notes;
-
-//     console.log(a)
-// }
-// function editNotesText(editButton) {
-//     var editTextPrompt = prompt("Enter notes for replacing notes");
-//     editButton.parentNode.parentNode.firstElementChild.firstElementChild.innerHTML = editTextPrompt;
-// }
-// function deleteNotes(deleteButton) {
-//     deleteButton.parentNode.parentNode.remove();
-// }
+const authCheck = () => {
+    const userUID = localStorage.getItem("uid")
+    if (!userUID) {
+        window.location.replace("./dashboard.html")
+    }
+}
 
 var input = document.getElementById("input");
 var notesParent = document.getElementById("notesParent");
@@ -82,3 +58,6 @@ function editNotes(editButton) {
     localStorage.setItem("key", JSON.stringify(getValue));
     displayNotes()
 }
+
+
+window.authCheck = authCheck;
